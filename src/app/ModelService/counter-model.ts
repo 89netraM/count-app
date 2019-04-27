c7ba5fe73e5744d14b4a0813f92825341d7353f6
Mårten Åsberg <marten.asberg@outlook.com>
2019-04-27 20:07:48 +0200

    Model and Serivce
    Model and accompanying service for handling them

---
+++ src/app/ModelService/counter-model.ts
@@ -0,0 +1,6 @@
+export interface CounterModel {
+	id: string;
+	name: string;
+	count: number;
+	color: number;
+}
---
+++ src/app/ModelService/model.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from "@angular/core";
+import { StorageService } from "../StorageService/storage.service";
+import { CounterModel } from "./counter-model";
+
+@Injectable({
+	providedIn: "root"
+})
+export class ModelService {
+	private static readonly keyIds: string = "counterIds";
+
+	public ids: Array<string> = null;
+	public counters: Array<CounterModel> = null;
+
+	public constructor(private storage: StorageService) { }
+
+	public async downloadAll(): Promise<void> {
+		await this.downloadAllIds();
+		await this.downloadAllCounters();
+	}
+	private async downloadAllIds(): Promise<void> {
+		if (await this.storage.has(ModelService.keyIds)) {
+			this.ids = (await this.storage.get(ModelService.keyIds)).split(",");
+		}
+		else {
+			this.ids = new Array<string>();
+		}
+	}
+	private async downloadAllCounters(): Promise<void> {
+		this.counters = new Array<CounterModel>();
+
+		if (this.ids != null) {
+			for (const id of this.ids) {
+				this.counters.push(this.fromString(await this.storage.get(id)));
+			}
+		}
+	}
+
+	public async saveAll(): Promise<void> {
+		await Promise.all([
+			this.saveAllIds(),
+			this.saveAllCounters()
+		]);
+	}
+	private async saveAllIds(): Promise<void> {
+		if (this.ids != null) {
+			await this.storage.set(ModelService.keyIds, this.ids.join(","));
+		}
+	}
+	private async saveAllCounters(): Promise<void> {
+		const promises: Array<Promise<void>> = new Array<Promise<void>>();
+
+		if (this.counters != null) {
+			for (const counter of this.counters) {
+				promises.push(this.saveCounter(counter));
+			}
+		}
+
+		await Promise.all(promises);
+	}
+	public async saveCounter(counter: CounterModel): Promise<void> {
+		await this.storage.set(counter.id, this.toString(counter));
+	}
+
+	public async addCounter(): Promise<void> {
+		const counter: CounterModel = {
+			id: this.UUID(),
+			name: "Counter",
+			count: 0,
+			color: 0
+		};
+
+		this.counters.push(counter);
+		this.ids.push(counter.id);
+
+		await Promise.all([
+			this.saveAllIds(),
+			this.saveCounter(counter)
+		]);
+	}
+
+	private fromString(s: string): CounterModel {
+		const p: Array<string> = s.split(",");
+
+		return {
+			id: p[0],
+			name: p[1],
+			count: parseInt(p[2], 16),
+			color: parseInt(p[3], 16)
+		};
+	}
+	private toString(c: CounterModel): string {
+		return c.id + "," + c.name + "," + c.count.toString(16) + "," + c.color.toString(16);
+	}
+
+	private UUID() {
+		return "xxxxxxxx-xxxx-4xxx-5xxx-xxxxxxxxxxxx".replace(/x/g, () => Math.floor(Math.random() * 16).toString(16));
+	}
+}
